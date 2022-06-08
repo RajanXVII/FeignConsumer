d248b68a5f985ff8190c4ce5aee9a07eef57d4a4
'RajanXVII' <a839348@DESKTOP-DN1RNVP>
2022-06-08 19:52:10 +0530

    Feign client changes

--- src/main/java/com/example/demo/FeignConsumerApplication.java
+++ src/main/java/com/example/demo/FeignConsumerApplication.java
@@ -1,17 +1,9 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.clients.CustomErrorHandler;
-import com.example.demo.clients.FeignClient;
-
-import feign.codec.ErrorDecoder;
 
 @SpringBootApplication
 @RestController
@@ -23,18 +15,7 @@ public class FeignConsumerApplication {
 		SpringApplication.run(FeignConsumerApplication.class, args);
 	}
 	
-	  @Autowired
-	  FeignClient feignClient;
-	
-	  @GetMapping("/test")
-	  public String home() {
-		  try {
-		  return feignClient.getHome(); 
-		  }
-		  catch(Exception e) {
-			  return "CAught exeception!!";
-		  }
-	  }
+	 
 	 
 	
 	
--- src/main/java/com/example/demo/clients/FeignClient.java
+++ src/main/java/com/example/demo/clients/FeignClient.java
@@ -3,8 +3,7 @@ package com.example.demo.clients;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-@org.springframework.cloud.openfeign.FeignClient(value="feignConsumer",url="http://localhost:8082",
-fallback = testFallback.class)
+@org.springframework.cloud.openfeign.FeignClient(value="feignConsumer",url="http://localhost:8082")
 @Component
 public interface FeignClient {
 	
