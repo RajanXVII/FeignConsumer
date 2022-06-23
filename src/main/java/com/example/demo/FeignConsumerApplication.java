d50bb9109d7aad2d7ea1f9010c0aeda256ef80a1
'RajanXVII' <a839348@DESKTOP-DN1RNVP>
2022-06-23 21:29:11 +0530

    Removing unncesessary dependecies.

--- src/main/java/com/example/demo/FeignConsumerApplication.java
+++ src/main/java/com/example/demo/FeignConsumerApplication.java
@@ -3,17 +3,14 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.clients.FeignClient;
 
-import feign.FeignException;
 
 @SpringBootApplication
 @RestController
-@EnableFeignClients
 public class FeignConsumerApplication {
 	
 	
@@ -30,7 +27,7 @@ public class FeignConsumerApplication {
 		  return feignClient.getHome(); 
 		  }
 		  
-		  catch(FeignException e) {
+		  catch(Exception e) {
 			  return "Generic CAught exeception!!";
 		  }
 	  }
